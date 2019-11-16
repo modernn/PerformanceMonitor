c308a051eeeaed2b5b9c62d88e986e4d9e81d3b7
Larson <lglarson@dhss.ak.local>
2019-11-15 16:13:52 -0900

    initial commit of basic .netcore 3.0 System Monitoring solution

---
+++ PerformanceMonitor/App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+
+public class App
+{
+    public static LogWriter myLogWriter = new LogWriter("Beginning the Logging of Computer Performance");
+    public static PerformanceCounter cpuCounter;
+    public static PerformanceCounter ramCounter;
+    public static int TotalRAM = 16281;
+
+    public static void Main()
+    {
+        cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        bool ShouldRun = true;
+        int cycleTime = 1000;
+        myLogWriter.LogWrite("Logging performance every "+cycleTime+"ms");
+        while(ShouldRun)
+        {
+            var myLogEntry = GetLogEntry();
+            Console.WriteLine(myLogEntry);
+            myLogWriter.LogWrite(myLogEntry);
+            Thread.Sleep(1000);
+        }
+    }
+    public static string GetLogEntry()
+    {
+        return DateTime.Now.ToString() + " RAM Used: " + GetRamUsage() + " CPU Used: " + GetCpuUsage();
+    }
+    public static string GetCpuUsage()
+    {
+        return Math.Round(cpuCounter.NextValue(),2) + "%";
+    }
+    public static string GetRamUsage()
+    {
+        float CurrentlyAvailiableRAM = ramCounter.NextValue();
+        float CurrentlyUsedRAM = TotalRAM - CurrentlyAvailiableRAM;
+        var CurrentUsedRamPercent = Math.Round(Math.Round(CurrentlyUsedRAM/TotalRAM, 3)*100,2);
+        return  "("+CurrentlyUsedRAM+"/"+TotalRAM+") "+CurrentUsedRamPercent+"%";
+    }
+}
---
+++ PerformanceMonitor/LogWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+//Logger from https://stackoverflow.com/questions/20185015/how-to-write-log-file-in-c 
+//Thank you BKSpurgeon
+ 
+public class LogWriter
+{
+    private string m_exePath = string.Empty;
+    public LogWriter(string logMessage)
+    {
+        LogWrite(logMessage);
+    }
+    public void LogWrite(string logMessage)
+    {
+        m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        try
+        {
+            using (StreamWriter w = File.AppendText(m_exePath + "\\" + "PerformanceMonitorlog "+DateTime.Now.Month+"-"+DateTime.Now.Day+"-"+DateTime.Now.Year+".txt"))
+            {
+                Log(logMessage, w);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+    public void Log(string logMessage, TextWriter txtWriter)
+    {
+        try
+        {
+            txtWriter.WriteLine(logMessage);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
